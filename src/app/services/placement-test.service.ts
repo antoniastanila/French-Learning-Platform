ec3156a57afd70b424eb811554acc9ae52b1c752
antoniastanila <antoniamaria.stanila@gmail.com>
2025-11-21 11:31:25 +0200

    Fix placement test API URL

--- src/app/services/placement-test.service.ts
+++ src/app/services/placement-test.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class PlacementTestService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getTestQuestions(level: string) {
-    return this.http.get<any[]>(`/api/exercises/placement-test/${level}`);
+    return this.http.get<any[]>(
+      `${environment.apiUrl}/api/exercises/placement-test/${level}`
+    );
   }
 }
